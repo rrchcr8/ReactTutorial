75888c2aad13038c471895498cb06d63c285313b
Rrch <rrch.cr8@gmail.com>
2021-02-13 01:21:48 -0400

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return (
+            <div className= "mt-5-flex justify-content-left">
+                This is the home page
+            </div>
+        )
+    }
+}
